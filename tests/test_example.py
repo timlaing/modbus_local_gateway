3596a3fcf1f484a911e02835eb35b3d60ae91544
Tim Laing <tim@laingcorp.co.uk>
2023-07-21 21:06:55 +0100

    Fixed testing

---
+++ tests/test_example.py
@@ -0,0 +1,10 @@
+"""Example Unit Test"""
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    """ "Example Test Case"""
+
+    def test_example(self):
+        """Example unit test"""
+        self.assertTrue(True)
