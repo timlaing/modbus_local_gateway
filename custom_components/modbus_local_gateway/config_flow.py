c7183ff1f188693ef6dad056478de818cae4dd60
Tim Laing <11019084+timlaing@users.noreply.github.com>
2025-03-25 22:57:10 +0000

    Ha updates (#42)
    * Update tasks and configuration
    * Fixed HA deprecations and other errors.

--- custom_components/modbus_local_gateway/config_flow.py
+++ custom_components/modbus_local_gateway/config_flow.py
@@ -38,10 +38,6 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 class OptionsFlowHandler(OptionsFlow):
     """Class to support the options flow"""
 
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry: ConfigEntry[Any] = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
@@ -180,6 +176,6 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
 
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
-        """Options flow callback"""
-        return OptionsFlowHandler(config_entry)
+    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlowHandler:
+        """Create the options flow."""
+        return OptionsFlowHandler()
