095e6c71da6933a7ffe28df9c53f16393430e193
Tim Laing <11019084+timlaing@users.noreply.github.com>
2025-03-26 08:23:32 +0000

    Update sensor.py
    Remove redundant import

--- custom_components/modbus_local_gateway/sensor.py
+++ custom_components/modbus_local_gateway/sensor.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import logging
-from datetime import datetime
 from typing import cast
 
 from homeassistant.components.sensor import RestoreSensor, SensorExtraStoredData
